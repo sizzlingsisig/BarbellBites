b7accb5d7d45a395394246da0b4311a6136bd952
Sizzlingsisig <herniachristianjoseph@gmail.com>
2026-02-21 00:56:42 +0800

    test: implemented manual REST api endpoint testing using Bruno

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 async function connectDB() {
   try {
     const uri = process.env.MONGO_URI;
-    const dbName = process.env.MONGO_DB_NAME || process.env.MONGO_DB || 'barbellbites';
+    const dbName = process.env.MONGO_DB_NAME || 'barbellbites';
 
     if (!uri) {
       throw new Error('MONGO_URI is not defined in environment variables');
