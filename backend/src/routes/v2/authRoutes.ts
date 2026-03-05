e4fae21d5f601c6e007fd03af0b16710ebe01f7b
Sherwin Paul Sabandal <slsabandal@up.edu.ph>
2026-03-05 18:26:02 +0800

    feat: import validation and add route

--- backend/src/routes/v2/authRoutes.ts
+++ backend/src/routes/v2/authRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import * as authController from '../../controllers/v2/authController.js'
 import { validate } from '../../middleware/validateRequests.js'
-import { RegisterRequest, LoginRequest } from '../../requests/v2/authRequests.js'
+import { RegisterRequest, LoginRequest, ForgotPasswordRequest } from '../../requests/v2/authRequests.js'
 import { protect } from '../../middleware/authMiddleware.js'
 
-const router = Router()
 
+const router = Router()
+router.post('/forgot-password', validate(ForgotPasswordRequest), authController.forgotPassword)
 router.post('/register', validate(RegisterRequest), authController.register)
 router.post('/login', validate(LoginRequest), authController.login)
 router.get('/test', protect, authController.getTest)
